ac06d00f0117b138e5cdcfd9eab37c0cabf735d8
lukas0409 <112852022+lukas0409@users.noreply.github.com>
2023-06-27 19:21:42 +0900

    🚦 신호기 코드 추가
    건널목과 2현시 신호기가 동시에 동작할 수 있도록 변경되었습니다.

--- railroad_crossing.ino
+++ railroad_crossing.ino
@@ -1,7 +1,8 @@
-/* 적외선센서, SG90을 이용한 철도건널목 아두이노 제어 
+/* 적외선센서, SG90을 이용한 철도건널목 아두이노 제어
 본 프로그램은 1종 건널목 기준으로 제작되었음
 차단기 (양측)
-적외선센서 필요
+적외선센서
+초록, 빨강 LED (신호기 디자인) 필요
 
 MP3모듈을 이용한 경보시스템은 미제작
 건널목 안내표지 설치필요
@@ -15,27 +16,32 @@ Servo myservo; // 서보모터 이름정의
 Servo myservo1; // 서보모터 이름정의
 int pos = 0; // 서보모터 각도
 int sensor = 8; // 센서 8번핀
+int red = 5;
+int green = 9;
 int val; // 
 
 void setup() {
-  // put your setup code here, to run once:
   myservo.attach(7); // SG90 핀지정
   myservo1.attach(11); // SG90 핀지정
   pinMode(sensor, INPUT); // 센서 입력지정
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
 }
 
 void loop() {
   val = digitalRead(sensor);  // 센서값 읽어옴
   
   if (val == 1) {          // 기차감지
+    digitalWrite(green, LOW);  // green 소등
+    digitalWrite(red, HIGH);   // red 점등
     myservo.write(90); // 건널목 차단
     myservo1.write(90); // 건널목 차단
-    delay(500);
   } 
 
   if (val == 0) { // 기차 감지X
     myservo.write(180); // 건널목 차단 해제
     myservo1.write(90); // 건널목 차단
-    delay(500);
+    digitalWrite(red, LOW);    // red 소등
+    digitalWrite(green, HIGH); // green 점등
   }
 }
